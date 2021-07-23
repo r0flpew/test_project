b2853fec24b8e075b91bf73b460e9ab0eadee141
r0flpew <71891970+r0flpew@users.noreply.github.com>
2021-07-23 14:39:13 +0400

    test_main_page and small fixes for browser fixture

--- conftest.py
+++ conftest.py
@@ -12,14 +12,17 @@ def pytest_addoption(parser):
 @pytest.fixture(scope="function")
 def browser(request):
     browser_name = request.config.getoption("browser_name")
-    #user_language = request.config.getoption("language")
+    user_language = request.config.getoption("language")
     driver = None
     if browser_name == "chrome":
         options = webdriver.ChromeOptions()
         options.add_experimental_option("excludeSwitches", ["enable-logging"])
+        options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
         driver = webdriver.Chrome(options=options)
     elif browser_name == "firefox" or browser_name == "ff":
-        driver = webdriver.Firefox()
+        profile = webdriver.FirefoxProfile()
+        profile.set_preference("intl.accept_languages", user_language)
+        driver = webdriver.Firefox(firefox_profile=profile)
     else:
         raise pytest.UsageError("--browser_name should be defined (chrome or firefox)")
 
