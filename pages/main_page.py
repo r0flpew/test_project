6d2b6dc4124cabc0e0dcfd2c046bc0b81b2f70bd
r0flpew <71891970+r0flpew@users.noreply.github.com>
2021-07-23 17:41:09 +0400

    test_main_page updated

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,10 +1,12 @@
 from .base_page import BasePage
+from .login_page import LoginPage
 from .locators import MainPageLocators
 
 class MainPage(BasePage):
     def go_to_login_page(self):
         login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
         login_link.click()
+        return LoginPage(browser=self.browser, url=self.browser.current_url) # возвращаем объект loginpage если он нужен
 
     def should_be_login_link(self):
         assert self.is_element_present(MainPageLocators.LOGIN_LINK), "user should see login_link"
